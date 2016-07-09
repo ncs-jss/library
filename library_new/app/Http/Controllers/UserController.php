09017cac9f455d0f860927fea719d1ab76305843
sahilkmaurya <sahil.kr.maurya@gmail.com>
2016-07-10 04:34:03 +0530

    New function added

--- library_new/app/Http/Controllers/UserController.php
+++ library_new/app/Http/Controllers/UserController.php
@@ -2,16 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\User;
 
+
+use Session;
+use Validator;
+use Redirect;
+use Auth;
+
+use Illuminate\Support\Facades\Input;
+use Illuminate\Http\Request;
 use App\Http\Requests;
 
 class UserController extends Controller
 {
-	public function root(){
-		return \View::make('welcome');
-	}
-    public function login(){
-    	return 1;
+
+    public function postLogin(){
+    	$data = Input::all();
+        array_pop($data);
+        $rules = ['username'=>'required',
+        'password'=> 'required'];
+
+        $validator = Validator::make($data, $rules);
+
+        if($validator->fails()){
+            return redirect('login')
+            ->withErrors($validator->errors())
+            ->withInput();
+        }
+        else{
+            if(\Auth::attempt($data)){
+                /*session_unset();
+                Session::put('username',$data['username']);
+                Session::save();
+                return redirect('home');*/print_r($data);
+            }else{/*
+                Session::flash('err',"1");
+                return redirect('contact');
+            */echo"sorry mate";}
+        }
     }
 }
