8571c810de78c230ec1bff686925aa0f235cdbd3
sahilkmaurya <sahil.kr.maurya@gmail.com>
2016-07-10 04:28:11 +0530

    Minor changes to models

--- library_new/app/Staff.php
+++ library_new/app/Staff.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Staff extends Model
 {
     protected $fillable = [
-        'id', 'name','username','email','phone_no','type','course','branch'
+         'name','username','email','phone_no','type','course','branch'
     ];
     protected $hidden = [
-        'created_at','updated_at','remember_token'
+        'remember_token'
     ];
     protected $table = 'staff_details';
 }
