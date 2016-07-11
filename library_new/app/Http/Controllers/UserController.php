f58a75ca98f9c390b31be77ddc0af7e01050a194
sahilkmaurya <sahil.kr.maurya@gmail.com>
2016-07-11 23:09:20 +0530

    New function added

--- library_new/app/Http/Controllers/UserController.php
+++ library_new/app/Http/Controllers/UserController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\User;
-
+use App\Queries;
+use App\Arrivals;
 
 use Session;
 use Validator;
@@ -11,6 +12,7 @@ use Redirect;
 use Auth;
 
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 
@@ -32,14 +34,54 @@ class UserController extends Controller
         }
         else{
             if(\Auth::attempt($data)){
-                /*session_unset();
+                session_unset();
                 Session::put('username',$data['username']);
                 Session::save();
-                return redirect('home');*/print_r($data);
-            }else{/*
+                return redirect('/');
+            }else{
                 Session::flash('err',"1");
-                return redirect('contact');
-            */echo"sorry mate";}
+                return view('admin-login',['err'=>"Username Or Password did not match."]);
+            }
+        }
+    }
+
+    public function postQuery(){
+        $data=Input::all();
+        array_pop($data);
+        $rules=['username'=>'required', 'query'=>'required', 'subject'=>'required'];
+        $validator=Validator::make($data,$rules);
+
+        if($validator->fails()){
+            return redirect('query')
+            ->withErrors($validator->errors())
+            ->withInput();
+        }else{
+            $query= new Queries;
+            $query->username = $data['username'];
+            $query->subject = $data['subject'];
+            $query->query = $data['query'];
+            $query->save();
+            return view('query',['err'=>"Your Query has been submitted successfully. Thank You!!"]);
+        }
+    }
+
+    public function postAddBook(){
+        $data=Input::all();
+        array_pop($data);
+        $rules=['book_id'=>'required', 'book_name'=>'required', 'book_desc'=>'required'];
+        $validator=Validator::make($data,$rules);
+
+        if($validator->fails()){
+            return redirect('add_book')
+            ->withErrors($validator->errors())
+            ->withInput();
+        }else{
+            $arrival= new Arrivals;
+            $arrival->book_id = $data['book_id'];
+            $arrival->book_name = $data['book_name'];
+            $arrival->book_desc = $data['book_desc'];
+            $arrival->save();
+            return view('add_book',['err'=>"The book has been added to the list."]);
         }
     }
 }
