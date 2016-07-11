6b46db0b9b6acc8505e8d006da602f867cf31f1e
sahilkmaurya <sahil.kr.maurya@gmail.com>
2016-07-11 23:09:05 +0530

    New function added

--- library_new/app/Http/Controllers/PagesController.php
+++ library_new/app/Http/Controllers/PagesController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Notices;
+use App\Arrivals;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -13,15 +14,19 @@ class PagesController extends Controller
 	}
 
     public function getLogin(){
-    	return view('admin-login');
+    	return view('admin-login')->with('err',"");
     }
 
     public function getArrivals(){
-    	return view('new-arrivals');
+        $arrivals=Arrivals::Orderby('id','des')->get();
+        $id=1;
+        return view('new-arrivals',['arrivals' => $arrivals,'id'=>$id]);
     }
 
     public function getNotices(){
-    	return view('notices');
+        $notices=Notices::Orderby('id','des')->get();
+        $id=1;
+    	return view('notices',['notices' => $notices,'id'=>$id]);
     }
 
     public function getSuggest(){
@@ -33,7 +38,7 @@ class PagesController extends Controller
     }
 
     public function getQuery(){
-    	return view('query');
+    	return view('query')->with('err',"");
     }
 
     public function getContact(){
@@ -47,5 +52,9 @@ class PagesController extends Controller
     public function getExternalLinks(){
     	return view('external-links');
     }
+
+    public function getAddBook(){
+         return view('add_book')->with('err',"");
+     }
 
 }
