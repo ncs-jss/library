bb89a487ab9ef6796ff924288d1efb110d65f3b8
sahilkmaurya <sahil.kr.maurya@gmail.com>
2016-07-22 01:11:40 +0530

    Custom Menu Accomodated

--- library_new/app/Http/Controllers/PagesController.php
+++ library_new/app/Http/Controllers/PagesController.php
@@ -22,20 +22,24 @@ use App\Http\Requests;
 class PagesController extends Controller
 {
 	public function getIndex(){
+        $menu=Menu::Orderby('id','des')->get();
         if(\Auth::Check()){
         $user = User::where('username', Session::get('username'))->first();
         return view('index')
         ->with('username',$user->username)
-        ->with('level',$user->level);
+        ->with('level',$user->level)
+        ->with('menu',$menu);
         }else{
         return view('index')
         ->with('username',"Guest")
-        ->with('level',3);
+        ->with('level',3)
+        ->with('menu',$menu);
         }
     }
 	
 
     public function getLogin(){
+        $menu=Menu::Orderby('id','des')->get();
         if(\Auth::Check()){
         $user = User::where('username', Session::get('username'))->first();
         return Redirect::back();
@@ -44,16 +48,19 @@ class PagesController extends Controller
             {
                 return view('admin-login',['err'=>"Username Or Password did not match."])
                 ->with('username',"Guest")
-                ->with('level',3);;
+                ->with('level',3)
+                ->with('menu',$menu);
             }else{
     	        return view('admin-login')
                 ->with('err',"")
                 ->with('username',"Guest")
-                ->with('level',3);
+                ->with('level',3)
+                ->with('menu',$menu);
             }
         }
     }
     public function getArrivals(){
+        $menu=Menu::Orderby('id','des')->get();
         $arrivals=Arrivals::Orderby('id','des')->get();
         $id=1;
         if(\Auth::Check()){
@@ -61,16 +68,19 @@ class PagesController extends Controller
         return view('new-arrivals',['arrivals' => $arrivals,
             'id'=>$id])
             ->with('username',$user->username)
-            ->with('level',$user->level);        
+            ->with('level',$user->level)
+            ->with('menu',$menu);        
         }else{
         return view('new-arrivals',['arrivals' => $arrivals,
             'id'=>$id])
             ->with('username',"Guest")
-            ->with('level',3);
+            ->with('level',3)
+            ->with('menu',$menu);
         }
     }
 
     public function getNotices(){
+        $menu=Menu::Orderby('id','des')->get();
         $notices=Notices::Orderby('id','des')->get();
         $id=1;
         if(\Auth::Check()){
@@ -78,67 +88,81 @@ class PagesController extends Controller
         return view('notices',['notices' => $notices,
             'id'=>$id])
             ->with('username',$user->username)
-            ->with('level',$user->level);
+            ->with('level',$user->level)
+            ->with('menu',$menu);
         }else{
         return view('notices',['notices' => $notices,
             'id'=>$id])
             ->with('username',"Guest")
-            ->with('level',3);
+            ->with('level',3)
+            ->with('menu',$menu);
         }
     }
 
 
     
     public function getServices(){
+        $menu=Menu::Orderby('id','des')->get();
         if(\Auth::Check()){
         $user = User::where('username', Session::get('username'))->first();
         return view('services')
         ->with('username',$user->username)
-        ->with('level',$user->level);
+        ->with('level',$user->level)
+        ->with('menu',$menu);
         }else{
         return view('services')
         ->with('username',"Guest")
-        ->with('level',3);
+        ->with('level',3)
+        ->with('menu',$menu);
         }
     }
 
     
     public function getContact(){
+        $menu=Menu::Orderby('id','des')->get();
         if(\Auth::Check()){
         $user = User::where('username', Session::get('username'))->first();
         return view('contact')
         ->with('username',$user->username)
-        ->with('level',$user->level);
+        ->with('level',$user->level)
+        ->with('menu',$menu);
         }else{
         return view('contact')
         ->with('username',"Guest")
-        ->with('level',3);
+        ->with('level',3)
+        ->with('menu',$menu);
         }
     }
 
     public function getResources(){
+        $menu=Menu::Orderby('id','des')->get();
         if(\Auth::Check()){
         $user = User::where('username', Session::get('username'))->first();
         return view('e-resources')
         ->with('username',$user->username)
-        ->with('level',$user->level);
+        ->with('level',$user->level)
+        ->with('menu',$menu);
         }else{
         return view('e-resources')
         ->with('username',"Guest")
-        ->with('level',3);
+        ->with('level',3)
+        ->with('menu',$menu);
         }
     }
 
     public function getExternalLinks(){
+        $menu=Menu::Orderby('id','des')->get();
         if(\Auth::Check()){
         $user = User::where('username', Session::get('username'))->first();
         return view('external-links')
         ->with('username',$user->username)
-        ->with('level',$user->level);
+        ->with('level',$user->level)
+        ->with('menu',$menu);
         }else{
         return view('external-links')
         ->with('username',"Guest")
-        ->with('level',3);
+        ->with('level',3)
+        ->with('menu',$menu);
         }
     }
 
@@ -150,8 +174,8 @@ class PagesController extends Controller
 
      
 
-    public function getNotice($id)
-    {
+    public function getNotice($id){
+        $menu=Menu::Orderby('id','des')->get();
         $notice = Notices::where('id',$id)->get()[0]->notice;   
         $subject = Notices::where('id',$id)->get()[0]->subject;
         if(\Auth::Check()){
@@ -159,7 +183,8 @@ class PagesController extends Controller
         return view('view_notice',['subject' => $subject, 
             'notice' => $notice])
             ->with('username',$user->username)
-            ->with('level',$user->level);
+            ->with('level',$user->level)
+            ->with('menu',$menu);
         }else{
         return redirect('login');
         }  
@@ -168,6 +193,7 @@ class PagesController extends Controller
     
 
     public function getViewBook($id){
+        $menu=Menu::Orderby('id','des')->get();
         $book = Arrivals::where('id',$id)->get()[0]->book_name;
         $book_id = Arrivals::where('id',$id)->get()[0]->book_id;
         $book_desc = Arrivals::where('id',$id)->get()[0]->book_desc;
@@ -177,7 +203,8 @@ class PagesController extends Controller
             'book_id' => $book_id,
             'book_desc' => $book_desc])
             ->with('username',$user->username)
-            ->with('level',$user->level);
+            ->with('level',$user->level)
+            ->with('menu',$menu);
         }else{
             return redirect('login');        
         }
@@ -185,18 +212,21 @@ class PagesController extends Controller
 
     
     public function getQuery(){
+        $menu=Menu::Orderby('id','des')->get();
         if(\Auth::Check()){
         $user = User::where('username', Session::get('username'))->first();
             if(Session::get('err') == '1')
             {
                 return view('query',['err'=>"Your Query has been submitted successfully. Thank You!!"])
                 ->with('username',$user->username)
-                ->with('level',$user->level);
+                ->with('level',$user->level)
+                ->with('menu',$menu);
             }else{
                 return view('query')
                 ->with('err',"")
                 ->with('username',$user->username)
-                ->with('level',$user->level);
+                ->with('level',$user->level)
+                ->with('menu',$menu);
             }
         }else{
         return redirect('login');
@@ -205,17 +235,20 @@ class PagesController extends Controller
 
 
     public function getSuggest(){
+        $menu=Menu::Orderby('id','des')->get();
         if(\Auth::Check()){
         $user = User::where('username', Session::get('username'))->first();
          if(Session::get('err') == '1'){
             return view('suggest',['err'=>"Your suggestion has been submitted."])
             ->with('username',$user->username)
-            ->with('level',$user->level);
+            ->with('level',$user->level)
+            ->with('menu',$menu);
          }else{
             return view('suggest')
             ->with('err',"")
             ->with('username',$user->username)
-            ->with('level',$user->level);
+            ->with('level',$user->level)
+            ->with('menu',$menu);
          }
         }else{
         return redirect('login');
@@ -224,6 +257,7 @@ class PagesController extends Controller
 
 // Debug
     public function getUserQueries(){
+        $menu=Menu::Orderby('id','des')->get();
         $queries = Queries::where('username', Session::get('username'))->get();
         $id=1;
 
@@ -233,7 +267,8 @@ class PagesController extends Controller
         ->with('username',$user->username)
         ->with('level',$user->level)
         ->with('queries',$queries)
-        ->with('id',$id);
+        ->with('id',$id)
+        ->with('menu',$menu);
         }else{
         return redirect('login');
         }
@@ -245,6 +280,12 @@ class PagesController extends Controller
         \Auth::logout();
         session_unset();
         return redirect('/');
+    }
+
+    public function getCustomMenu($name){
+        $menu=Menu::Orderby('id','des')->get();
+        $content= Menu::where('menuname',$name)->get()[0]->content;
+        echo $content;
     }
 
 }
