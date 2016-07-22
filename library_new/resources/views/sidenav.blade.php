77934bffad4609d2c77a9114f6783220a3422720
sahilkmaurya <sahil.kr.maurya@gmail.com>
2016-07-22 16:48:38 +0530

    Menu item Added

--- library_new/resources/views/sidenav.blade.php
+++ library_new/resources/views/sidenav.blade.php
@@ -55,6 +55,7 @@
                                 <li class="list-group-item dropdown-toggle" ><a href="{{route('view_menus')}}">View Menu Item</a></li>
                                 <li class="list-group-item dropdown-toggle" ><a href="{{route('add_notices')}}">Add Notices</a></li>
                                 @endif 
+                                <li class="list-group-item dropdown-toggle" ><a href="{{route('view_papers')}}">View Question Papers</a></li>
                             </ul>
                           </div>
 
