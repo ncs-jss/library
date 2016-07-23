5fa6a0ac5b67b10c30585ea34f27c8de8fff6cf3
sahilkmaurya <sahil.kr.maurya@gmail.com>
2016-07-23 14:49:20 +0530

    Migrations added and modified

--- library_new/database/migrations/2016_07_23_091326_create_papers_table.php
+++ library_new/database/migrations/2016_07_23_091326_create_papers_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMenuTable extends Migration
+class CreatePapersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,10 @@ class CreateMenuTable extends Migration
      */
     public function up()
     {
-        Schema::create('menu', function (Blueprint $table) {
+        Schema::create('notices', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('menu_name')->length(25)->unique();
-            $table->string('content')->length(20000);
-            $table->tinyInteger('status');
+            $table->string('name')->length(100);
+            $table->string('path')->length(1000);
             $table->rememberToken();
             $table->timestamps();
         });
@@ -29,6 +28,6 @@ class CreateMenuTable extends Migration
      */
     public function down()
     {
-        Schema::drop('menu');
+        Schema::drop('papers');
     }
 }
