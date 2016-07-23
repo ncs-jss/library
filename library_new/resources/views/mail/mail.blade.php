216b4c717fd38213923e940b420ac60fb222327f
sahilkmaurya <sahil.kr.maurya@gmail.com>
2016-07-23 22:38:42 +0530

    Mail View added

---
+++ library_new/resources/views/mail/mail.blade.php
@@ -0,0 +1,3 @@
+<h1>Hey, {{ $name }}</h1>
+<p>This is to Inform you that, the book {{ $book_name }} has been added to the college's library. 
+For more information visit the College <a href="#">Library Website</a> or the library itself.</p>
